01814ccf9bc734b4e4b55b5368b48004742f23a8
黄砷才 <shencai.huang@hand-china.com>
2019-09-02 23:08:41 +0800

    update

--- pages/share/index/index.js
+++ pages/share/index/index.js
@@ -5,7 +5,9 @@ Component({
     addGlobalClass: true,
   },
   data: {
-    photoWidth: (wx.getSystemInfoSync().windowWidth * 0.88 * 0.9 -30)/ 3,
+    onePhotoWidth: wx.getSystemInfoSync().windowWidth * 0.88 * 0.85 * 0.8,
+    twoPhotoWidth: (wx.getSystemInfoSync().windowWidth * 0.88 * 0.85 - 20) * 0.9 / 2,
+    threePhotoWidth: (wx.getSystemInfoSync().windowWidth * 0.88 * 0.85 - 35) / 3,
     circleList: [],
     userInfo: {},
     previewImageList: []
@@ -14,19 +16,35 @@ Component({
     this.setData({
       userInfo: app.globalData.userInfo
     })
-    console.info(this.data.userInfo)
+    wx.showLoading({
+      title: 'loading',
+    })
     wx.request({
       url: 'https://easy-mock.com/mock/5d6cd16f0f2596069bb1b409/v1/share/list',
       success: res => {
         this.setData({
           circleList: res.data
         })
+        wx.hideLoading()
       }
     })
   },
   methods: {
+    loadMoreData (e) {
+      wx.showLoading({
+        title: 'loading',
+      })
+      wx.request({
+        url: 'https://easy-mock.com/mock/5d6cd16f0f2596069bb1b409/v1/share/list',
+        success: res => {
+          this.setData({
+            circleList: this.data.circleList.concat(res.data)
+          })
+          wx.hideLoading()
+        }
+      })
+    },
     bindImgPreview(e) {
-      console.info(e.target.dataset)
       let currentPicture = e.target.dataset.url
       let index = e.target.dataset.index
       if (this.data.circleList[index].pictureList) {
